25ffd919194b957db5da58af92d1c058d8bc857c
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2021-02-04 19:43:49 -0500

    Initial Commit

---
+++ Shared/TravelJournal2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  TravelJournal2_0App.swift
+//  Shared
+//
+//  Created by Fulcherberguer, Fernanda on 2021-02-04.
+//
+
+import SwiftUI
+
+@main
+struct TravelJournal2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
